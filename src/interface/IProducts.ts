6073e93e69f0981f8791ce9a6cc6b8d237b3daa0
leobraboo <leonardo.l.souza@ufv.br>
2022-11-25 16:23:20 -0300

    FEATURE : interface produtos

---
+++ src/interface/IProducts.ts
@@ -0,0 +1,13 @@
+export interface IProducts {
+  id: string
+  idEmpresa?: string
+  nome?: string
+  valor?: number | string
+  valorVenda?: number | string
+  unidade?: string
+  descricao?: string
+  codBarras?: string
+  codReferencia?: string
+  estoque?: number | string
+  estoqueMin?: number | string
+}
