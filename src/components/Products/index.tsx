b07e3764ee1701fc6fba7b21b3f2be54ae9edc6c
leobraboo <leonardo.l.souza@ufv.br>
2022-12-25 21:10:38 -0300

    feat : icons + tab pills

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Form, Tabs, Tab , Stack } from 'react-bootstrap';
+import { Button, Card, Form, Tabs, Tab, Stack, Row, Col, Nav } from 'react-bootstrap';
 import { RegisterProduct } from './RegisterProduct';
 import { ContentProduct } from './styles';
 
@@ -7,20 +7,33 @@ export function CProducts() {
     <ContentProduct>
       <Card style={{ marginTop: "90px", width: "80%", height: "80vh" }}>
         <Card.Body>
-          <Tabs
-            defaultActiveKey="register"
-            className="mb-3"
-          >
-            <Tab eventKey="register" title="Cadastro">
-              <RegisterProduct />
-            </Tab>
-            <Tab eventKey="search" title="Pesquisar">
-              <Stack direction="horizontal" gap={3}>
-                <Form.Control className="me-auto" placeholder="Pesquisar" />
-                <Button variant="secondary">Pesquisar</Button>
-              </Stack>
-            </Tab>
-          </Tabs>
+          <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+            <Row>
+              <Col sm={3}>
+                <Nav variant="pills" className="flex-column">
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Cadastro</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Pesquisar</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col sm={9}>
+                <Tab.Content>
+                  <Tab.Pane eventKey="first">
+                    <RegisterProduct />
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="second">
+                    <Stack direction="horizontal" gap={3}>
+                      <Form.Control className="me-auto" placeholder="Pesquisar" />
+                      <Button variant="secondary">Pesquisar</Button>
+                    </Stack>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
         </Card.Body>
       </Card>
     </ContentProduct >
