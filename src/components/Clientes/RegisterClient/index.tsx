519dcbb83dde100177535b9da7feee273def4645
leobraboo <leonardo.l.souza@ufv.br>
2022-12-26 16:10:42 -0300

    handlechange

--- src/components/Clientes/RegisterClient/index.tsx
+++ src/components/Clientes/RegisterClient/index.tsx
@@ -1,43 +1,54 @@
+import { useContext } from 'react';
 import { Button, Col, Row, Card, Form } from 'react-bootstrap';
+import { IClient } from '../../../interface/IClient';
+import { ClientContext } from '../../../shared/Context/ClientContext';
+import { GlobalContext } from '../../../shared/Context/globalContext';
 import { ContentButton } from '../styles';
 
 export function RegisterClient() {
+  const { handleChange } = useContext( ClientContext ) as { handleChange: React.ChangeEventHandler }
+
+  const { client } = useContext(GlobalContext) as { client: IClient }
+
   return (
     <Card style={{ width: "100%", height: "auto" }}>
       <Card.Body>
         <Form>
+        <Form.Group as={Col}>
+              <Form.Control onChange={handleChange} hidden name='id' value={client.id || ""} type="text" />
+            </Form.Group>
           <Row>
             <Form.Group as={Col}>
               <Form.Label>Nome</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control onChange={handleChange} name='nome' value={client.nome || ""} type="text" />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>CPF/CNPJ</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control onChange={handleChange} name='cpfCnpj' value={client.cpfCnpj || ""} type="text" />
             </Form.Group>
           </Row>
           <Form.Group>
             <Form.Label>Endereço</Form.Label>
-            <Form.Control />
+            <Form.Control onChange={handleChange} name='endereco' value={client.endereco || ""} />
           </Form.Group>
           <Row>
             <Form.Group as={Col}>
               <Form.Label>Numero</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={handleChange} name='numero' value={client.numero || ""} />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Complemento</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={handleChange} name='complemento' value={client.complemento || ""} />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Bairro</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={handleChange} name='bairro' value={client.bairro || ""} />
             </Form.Group>
           </Row>
           <Row>
             <Form.Group as={Col}>
               <Form.Label>Cidade</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={handleChange} name='cidae' value={client.cidade || ""} />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Estado</Form.Label>
@@ -48,21 +59,21 @@ export function RegisterClient() {
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>CEP</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={handleChange} name='cep' value={client.cep || ""} />
             </Form.Group>
           </Row>
           <Row className="mb-3">
             <Form.Group as={Col}>
               <Form.Label>Telefone</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={handleChange} name='telefone' value={client.telefone || ""} />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Celular</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={handleChange} name='celular' value={client.celular || ""} />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Email</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={handleChange} name='email' value={client.email || ""} />
             </Form.Group>
           </Row>
           <ContentButton>
--- src/shared/Context/ClientContext.tsx
+++ src/shared/Context/ClientContext.tsx
@@ -1,5 +1,21 @@
-function ClientProvider() {
-  return(
-    <></>
+import { createContext } from "react";
+import { UseClient } from "../../shared/Hooks/useClient";
+
+interface AuxProps {
+  children: JSX.Element[] | JSX.Element
+}
+
+const ClientContext = createContext({})
+
+function ClientProvider({ children }: AuxProps) {
+  const { handleChange } = UseClient()
+
+  return (
+    <ClientContext.Provider value={{ handleChange }}>
+      {children}
+    </ClientContext.Provider>
   )
 }
+
+
+export { ClientProvider, ClientContext }
